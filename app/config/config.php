ef6849597245ab3775c8d2fd9db16d6e093d6ced
Drummond-Dev <60481837+Drummond-Dev@users.noreply.github.com>
2021-06-10 23:40:32 -0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'admin');
-define('DB_PASS', 'dd4417');
-define('DB_NAME', 'ipmvc');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
+define('DB_NAME', 'bd_name');
 
 // App Root
 define('APP_ROOT', dirname(dirname(__FILE__)));
 // URL Root
 define('URL_ROOT', 'http://localhost/ipmvc');
 // Site Name
-define('SITE_NAME', 'iProduceMVC');
+define('SITE_NAME', 'Nome do site');
